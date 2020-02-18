f054c14f5615cc894a094fe1ed4d0c854d9d5887
dhen2121 <50294113+dhen2121@users.noreply.github.com>
2020-02-18 18:41:47 +0530

    Add files via upload

---
+++ GreensTech.java
@@ -0,0 +1,38 @@
+package org.greens.bcbsaz.branches;
+
+public class GreensTech {
+
+	public void greensOMR() {
+	
+		System.out.println("OMR Branch");
+}
+
+	public void greensAdayar() {
+	
+		System.out.println("Adayar Branch");
+	}
+
+	public void greensTambaram() {
+		
+		System.out.println("Tambaram Branch");
+	}
+	
+	public void greensVelacherry() {
+		
+		System.out.println("Velacherry Branch");
+	}
+	
+	public void greensAnnaNagar() {
+		
+		System.out.println("AnnaNagar Branch");
+	}
+	
+public static void main(String[] args) {
+	GreensTech GT = new GreensTech();
+	GT.greensOMR();
+	GT.greensAdayar();
+	GT.greensTambaram();
+	GT.greensVelacherry();
+	GT.greensAnnaNagar();
+}	
+}
